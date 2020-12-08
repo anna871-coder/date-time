f9a33bba78fef8df20f77a87c133b7711688d96a
Anna <szepvolgyi.ann@gmail.com>
2020-12-08 16:18:06 +0100

    modified main.js

--- main.js
+++ main.js
@@ -2,7 +2,10 @@ function pageLoad() {
 
     let inputElement = document.querySelector("#date");
     let dateValue = new Date();
-    let actualDate = dateValue.getFullYear() + "-" + (dateValue.getMonth() + 1) + "-" + (dateValue.getDate() + "").padStart(2, "0");
+    let year = dateValue.getFullYear();
+    let month = (dateValue.getMonth() + 1 + "").padStart(2, "0");
+    let day = (dateValue.getDate() + "").padStart(2, "0");
+    let actualDate = `${year}-${month}-${day}`;
     console.log(actualDate);
     inputElement.setAttribute("min", actualDate);
 }
